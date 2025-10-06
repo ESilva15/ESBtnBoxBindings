527ed941f326582a640d3c272c64a77ca7783c7e
Eduardo Silva <eduardosilva@disroot.org>
2025-10-06 04:07:37 +0100

    Added transfercaseControls

--- lua/vehicle/extensions/ESBtnBoxDrivetrain.lua
+++ lua/vehicle/extensions/ESBtnBoxDrivetrain.lua
@@ -1,5 +1,47 @@
 local M = {}
 
+----------------------------------------------------------------------------------------------------
+-- Range Box
+----------------------------------------------------------------------------------------------------
 
+local rangeMode4hi = "4hi"
+local rangeMode4lo = "4lo"
+local rangeMode2hi = "2hi"
+local curRangeMode = rangeMode4hi
+
+local function toggleHighFour()
+  -- local transferCase = controller.getControllersByType('transfercaseControl')
+  local transferCase = controller.getController('transfercaseControl')
+  if transferCase then
+    if curRangeMode == rangeMode4hi then
+      print("Setting 2lo")
+      transferCase.setDriveMode(rangeMode2hi)
+      curRangeMode = rangeMode2hi
+    else
+      print("Setting 4hi")
+      transferCase.setDriveMode(rangeMode4hi)
+      curRangeMode = rangeMode4hi
+    end
+  end
+end
+
+local function toggleLowFour()
+  -- local transferCase = controller.getControllersByType('transfercaseControl')
+  local transferCase = controller.getController('transfercaseControl')
+  if transferCase then
+    if curRangeMode == rangeMode4lo then
+      print("Setting 2lo")
+      transferCase.setDriveMode(rangeMode2hi)
+      curRangeMode = rangeMode2hi
+    else
+      print("Setting 4lo")
+      transferCase.setDriveMode(rangeMode4lo)
+      curRangeMode = rangeMode4lo
+    end
+  end
+end
+
+M.toggleHighFour = toggleHighFour
+M.toggleLowFour = toggleLowFour
 
 return M
